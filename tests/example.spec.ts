39d5a01a0f4509c115d88f680e66c75301c7803a
lerabratko <bratkovaleriia@gmail.com>
2025-07-11 19:10:25 +0200

    example spec improvement

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,6 +4,7 @@ test('Простой тест — проверка заголовка', async ({
     await page.goto('https://example.com'); 
 
     await expect(page).toHaveTitle('Example Domain'); 
+    console.log("this is first commit in the new branch")
 }); 
 
 
